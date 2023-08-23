dc6fba702fef274893a3daf8faf19f35cd660865
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2023-08-23 11:52:36 +0200

    updated to 2.1

--- pyroll/elastic_mill_spring.py
+++ pyroll/elastic_mill_spring.py
@@ -1,42 +1,25 @@
-import logging
 from pyroll.core import RollPass, Hook, root_hooks
 
-VERSION = "2.0.0"
-
-log = logging.getLogger(__name__)
+VERSION = "2.1"
 
 RollPass.stand_stiffness = Hook[float]()
 """Stiffness of the roll stand of the current roll pass."""
 
-RollPass.gap_offset = Hook[float]()
-"""Offset between set and real roll gap."""
-
 
 @RollPass.stand_stiffness
 def stand_stiffness(self: RollPass):
-    raise ValueError("You must provide a roll_stand_stiffness to use the pyroll-elastic-mill-spring plugin.")
+    raise ValueError("You must provide a roll stand stiffness to use the pyroll-elastic-mill-spring plugin.")
 
+@RollPass.gap(wrapper=True)
+def gap(self: RollPass, cycle):
+    if cycle:
+        return None
 
-@RollPass.gap_offset
-def gap_offset(self: RollPass):
-    """Calculates the roll gap offset."""
+    rigid_gap = (yield)
+    elastic_gap_offset = self.roll_force / self.stand_stiffness
 
-    if not self.has_set_or_cached("roll_force"):
-        offset = 0
-    else:
-        offset = self.roll_force / self.stand_stiffness
-        log.info(f"Calculated a roll gap offset of {offset * 1e3:.2f} mm!")
-
-    return offset
-
-
-@RollPass.gap
-def gap(self: RollPass):
-    if not hasattr(self, "nominal_gap"):
-        self.nominal_gap = self.gap
-
-    gap = self.nominal_gap + self.gap_offset
-    return gap
+    elastic_gap = rigid_gap + elastic_gap_offset
+    return elastic_gap
 
 
 root_hooks.add(RollPass.gap)
